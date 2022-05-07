93c4add4a1a14707b6f9284fb04bcc04afc8cccb
arunrmdu <arunr.mdu@gmail.com>
2022-05-07 15:22:28 +0530

    inventory api

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -1,9 +1,15 @@
 from flask_restful import Api
 from app import app
-from .inventoryAPI import getAllProduct,getSubCategoryById
+from .inventoryAPI import getAllNewProduct, getAllPopularProduct, getAllProduct, getProductByCatID,getProductByParentCatID
 
 
 restServerInstance = Api(app)
 
 restServerInstance.add_resource(getAllProduct,"/api/v1.0/allProducts/")
+restServerInstance.add_resource(getAllPopularProduct,"/api/v1.0/allPopularProduct/")
+restServerInstance.add_resource(getAllNewProduct,"/api/v1.0/allNewProduct/")
+restServerInstance.add_resource(getProductByCatID,"/api/v1.0/productByCategoryID/<string:cat_id>")
+restServerInstance.add_resource(getProductByParentCatID,"/api/v1.0/productByp_CategoryID/<string:cat_id>")
+# restServerInstance.add_resource(getAllProduct,"/api/v1.0/allProducts/")
+# restServerInstance.add_resource(getAllProduct,"/api/v1.0/allProducts/")
 # restServerInstance.add_resource(getSubCategoryById,"/api/v1.0/subCategory/<string:cat_id>")
--- inventory/inventoryAPI.py
+++ inventory/inventoryAPI.py
@@ -1,3 +1,5 @@
+from calendar import c
+from datetime import datetime, timedelta
 from distutils.command.config import config
 from turtle import back
 from flask_restful import Resource
@@ -34,16 +36,49 @@ class getAllProduct(Resource):
         response=obj1.read()
         return response,200
 
-class getSubCategoryById(Resource):
-    def get(self,cat_id):
-        data={"filter":{"active":"Y","p_cat":cat_id},"field":{"cat":1}}
+
+class getAllPopularProduct(Resource):
+    def get(self):
+        data={"filter":{"active":"Y","is_popular":"Y"}}
         data['database']="portal"
-        data['collection']="category"
+        data['collection']="inventory"
         obj1 = MongoAPI(data)
         response=obj1.read()
         return response,200
 
+class getAllNewProduct(Resource):
+    def get(self):
+        to_date=datetime.now()-timedelta(int(config['generic']['to_date_for_new_product']))
+        data={"filter":{"active":"Y",'ins_dt': {'$gt': to_date}}}
+        data['database']="portal"
+        data['collection']="inventory"
+        obj1 = MongoAPI(data)
+        response=obj1.read()
+        return response,200
 
+class getProductByCatID(Resource):
+    def get(self,cat_id):
+        data={"filter":{"active":"Y","cat_id":cat_id}}
+        data['database']="portal"
+        data['collection']="inventory"
+        obj1 = MongoAPI(data)
+        response=obj1.read()
+        return response,200
+
+class getProductByParentCatID(Resource):
+    def get(self,cat_id):
+        data={"filter":{"active":"Y","p_cat":cat_id},"field":{"_id":1},"database":"portal","collection":"category"}
+        obj1 = MongoAPI(data)
+        c_ids=[]
+        for x in obj1.read():
+            c_ids.append(str(x['_id']))
+        if c_ids ==[]:
+            data={"filter":{"active":"Y","cat_id":cat_id},"database":"portal","collection":"inventory"}
+        else:
+            data={"filter":{"active":"Y","cat_id":{'$in':c_ids}},"database":"portal","collection":"inventory"}
+        obj1 = MongoAPI(data)
+        response=obj1.read()
+        return response,200
 
 
 
